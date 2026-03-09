a26e1f3bf83dc6288151993f20ac374775a254d9
Simone "Freddy" Faudarole <s.faudarole.3082@vallauri.edu>
2026-03-09 17:37:46 +0100

    Merge branch 'main' of https://github.com/Stefano-Tomatis/Progetto-TPSIT-5a

--- angular/clientSide/src/app/modulo-http.service.ts
+++ angular/clientSide/src/app/modulo-http.service.ts
@@ -20,4 +20,21 @@ export class ModuloHttpService {
    loginAsAdmin(user:any):Observable<any>{
     return this.http.post('http://localhost:3000/session/login/admin', user); // da verificare endpoint
   }
+
+  getVisite(dataInizio:any, dataFine:any):Observable<any>{
+    dataInizio = this.toDatabaseDateTime(dataInizio);
+    dataFine = this.toDatabaseDateTime(dataFine);
+    return this.http.get(`http://localhost:3000/db/private/visits/externalDoctor?dateStart=${dataInizio}&dateEnd=${dataFine}`);
+  }
+
+  toDatabaseDateTime(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+  }
 }
